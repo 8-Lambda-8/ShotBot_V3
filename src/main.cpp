f2b298bef4dcf465167860c4b1825cb600c94766
8-Lambda-8 <j.wasle111@gmail.com>
2020-03-23 23:28:29 +0100

    added Coordinate Calculation

--- src/main.cpp
+++ src/main.cpp
@@ -83,7 +83,23 @@ void stepper_off()
 	digitalWrite(EN_PIN, HIGH);
 }
 
-void setup() {
+const uint8_t Xcnt = 6;
+const uint8_t Ycnt = 4;
+
+int posY_(uint8_t id){
+	return (int)id/(int)Xcnt;
+}
+int posY(uint8_t id){
+	return posGap*posY_(id);
+}
+int posX(uint8_t id){
+
+	return posGap*(((posY_(id)%2)==0)	?
+		(id-(Xcnt)*posY_(id))	:	((1+posY_(id))*Xcnt-1-id));
+
+}
+
+void setup(){
 	#ifdef TARGET_ESP32
 		Serial.begin(115200);
 	#endif
